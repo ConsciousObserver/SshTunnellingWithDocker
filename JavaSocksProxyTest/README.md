3aa14bbcd315a7b236885f4c0280e641979e76dc
noname <nomail@nomail.com>
2023-05-11 21:12:08 +0530

    Added Java project for SOCKs testing

---
+++ JavaSocksProxyTest/README.md
@@ -0,0 +1,7 @@
+# Getting Started
+
+This Project provides an Apache docker contaienr that we can SSH to using user 'foo' and password 'bar'
+
+This is used to test SSH tunnelling and proxy.
+
+A Spring Boot java project is provided to test SOCKS proxy.
---
+++ JavaSocksProxyTest/src/main/java/com/example/demo/SocksProxyTestApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import java.net.URI;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SocksProxyTestApplication {
+
+    public static void main(String[] args) throws Exception {
+        log.info("Started ---------------");
+
+//        System.getProperties().put( "proxySet", "true" );
+//        System.getProperties().put( "socksProxyHost", "127.0.0.1" );
+//        System.getProperties().put( "socksProxyPort", "8080" );
+        
+        
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response = restTemplate.exchange(new URI("https://google.com"), HttpMethod.GET, new HttpEntity<Object>(new HttpHeaders()),
+                String.class);
+        
+        log.info("Response======================================================:\n\n{}", response.getBody());
+    }
+
+}
